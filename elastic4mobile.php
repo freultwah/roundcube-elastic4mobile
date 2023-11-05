f6002c28578ed19120ac97b1361011b214639968
freultwah <631910+freultwah@users.noreply.github.com>
2023-11-05 11:46:14 +0200

    PHP 8.2 changes as per ChatGPT recommendations

--- elastic4mobile.php
+++ elastic4mobile.php
@@ -3,7 +3,7 @@
 /**
  * Elastic skin for mobile devices plugin
  *
- * Selects the elastic skin if a mobile device is detected as client.
+ * Selects the elastic skin if a mobile device is detected as the client.
  *
  * @author Thomas Bruederli <thomas@roundcube.net>
  * @license GNU GPLv3+
@@ -15,18 +15,23 @@ class elastic4mobile extends rcube_plugin
     public function init()
     {
         $rcmail = rcmail::get_instance();
-        $detect = new Mobile_Detect;
 
-        if ($detect->isMobile() || $detect->isTablet()) {
-          $skin = 'elastic';
-          $rcmail->default_skin = $skin;
-          $rcmail->config->set('skin', $skin);
-          $rcmail->output->set_skin($skin);
+        // Check if the Mobile_Detect class exists
+        if (class_exists('Mobile_Detect')) {
+            $detect = new Mobile_Detect;
 
-          // disable skin switch as this wouldn't have any effect
-          $dont_override = $rcmail->config->get('dont_override', []);
-          $dont_override[] = 'skin';
-          $rcmail->config->set('dont_override', $dont_override);
+            if ($detect->isMobile() || $detect->isTablet()) {
+                $skin = 'elastic';
+                $rcmail->default_skin = $skin;
+                $rcmail->config->set('skin', $skin);
+                $rcmail->output->set_skin($skin);
+
+                // Disable skin switch as this wouldn't have any effect
+                $dont_override = $rcmail->config->get('dont_override', []);
+                $dont_override[] = 'skin';
+                $rcmail->config->set('dont_override', $dont_override);
+            }
         }
     }
 }
+
